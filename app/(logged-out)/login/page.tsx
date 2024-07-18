c9e1ba89d48dfaf9351ca10e81fff9d9a9748edc
dk649 <58308212+dk649@users.noreply.github.com>
2024-07-18 14:53:42 +0100

    update
    added login form

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -13,7 +13,37 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { PersonStandingIcon } from "lucide-react";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
 const LoginPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = () => {
+    console.log("loginin");
+  };
   return (
     <>
       <PersonStandingIcon size={50} />
@@ -23,7 +53,55 @@ const LoginPage = () => {
           <CardDescription>Login to the IT Support Dashboard</CardDescription>
         </CardHeader>
 
-        <CardContent>Login Form</CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is the email address you will signin with
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your password"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
+        </CardContent>
         <CardFooter className="justify-between">
           <small>Don't have an accout?</small>
           <Button asChild variant="outline" size="sm">
