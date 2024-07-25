36b4ef73cda61fe63bc7e8254ac5ec6c3d327f16
dk649 <58308212+dk649@users.noreply.github.com>
2024-07-25 15:10:04 +0100

    update
    calendar update 2

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import * as React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { DayPicker, useDayPicker, useNavigation } from "react-day-picker";
+import { format } from "date-fns";
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger } from "./select";
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
   className,
@@ -23,7 +25,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -51,16 +53,79 @@ function Calendar({
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
+        caption_dropdowns: "flex gap-2",
         ...classNames,
       }}
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Dropdown: (dropdownprops) => {
+          const { currentMonth, goToMonth } = useNavigation();
+          // console.log({ dropdownprops });
+          const { fromYear, fromMonth, fromDate, toYear, toMonth, toDate } =
+            useDayPicker();
+          let selectValues: { value: string; label: string }[] = [];
+          if (dropdownprops.name === "months") {
+            selectValues = Array.from({ length: 12 }, (_, i) => {
+              return {
+                value: i.toString(),
+                label: format(new Date(new Date().getFullYear(), i, 1), "MMM"),
+              };
+            });
+          } else if (dropdownprops.name === "years") {
+            const earliesYear =
+              fromYear || fromMonth?.getFullYear() || fromDate?.getFullYear();
+            const latestYear =
+              toYear || toMonth?.getFullYear() || toDate?.getFullYear();
+
+            if (earliesYear && latestYear) {
+              const yearsLength = latestYear - earliesYear;
+
+              selectValues = Array.from({ length: yearsLength + 1 }, (_, i) => {
+                return {
+                  value: (earliesYear + i).toString(),
+                  label: (earliesYear + i).toString(),
+                };
+              });
+            }
+          }
+
+          const caption = format(
+            currentMonth,
+            dropdownprops.name === "months" ? "MMM" : "yyyy"
+          );
+
+          return (
+            <Select
+              onValueChange={(newValue) => {
+                if (dropdownprops.name === "month") {
+                  const newDate = new Date(currentMonth);
+                  newDate.setMonth(parseInt(newValue));
+                  goToMonth(newDate);
+                } else if (dropdownprops.name === "years") {
+                  const newDate = new Date(currentMonth);
+                  newDate.setFullYear(parseInt(newValue));
+                  goToMonth(newDate);
+                }
+              }}
+              value={dropdownprops.value?.toString()}
+            >
+              <SelectTrigger>{caption}</SelectTrigger>
+              <SelectContent>
+                {selectValues.map((selectValue) => (
+                  <SelectItem key={selectValue.value} value={selectValue.value}>
+                    {selectValue.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          );
+        },
       }}
       {...props}
     />
-  )
+  );
 }
-Calendar.displayName = "Calendar"
+Calendar.displayName = "Calendar";
 
-export { Calendar }
+export { Calendar };
